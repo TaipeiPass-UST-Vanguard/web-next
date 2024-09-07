8d5340405fb120487ec4194dbe48eec5d1a3001b
an920107 <an920107@gmail.com>
2024-09-08 02:46:31 +0800

    fix: user and position context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Noto_Sans_TC } from "next/font/google";
 import "./globals.css";
+import { UserProvider } from "./_context/userContext";
+import PositionProvider from "./_context/positionContext";
 
 const fontSans = Noto_Sans_TC({
   subsets: ['latin'],
@@ -20,7 +22,11 @@ export default function RootLayout({
   return (
     <html lang="zh">
       <body className={fontSans.className}>
-        {children}
+        <UserProvider>
+          <PositionProvider>
+            {children}
+          </PositionProvider>
+        </UserProvider>
       </body>
     </html>
   );
